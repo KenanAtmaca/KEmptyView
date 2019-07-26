cf5e5ea2eaca953f3d3348a22be3ebd2d228e4eb
KenanAtmaca <mail.kenanatmaca@gmail.com>
2019-07-26 09:51:45 +0300

    * Update *

---
+++ KEmptyTable/Pods/Target Support Files/KEmptyView/KEmptyView-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_KEmptyView : NSObject
+@end
+@implementation PodsDummy_KEmptyView
+@end
---
+++ KEmptyTable/Pods/Target Support Files/Pods-KEmptyTable/Pods-KEmptyTable-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_KEmptyTable : NSObject
+@end
+@implementation PodsDummy_Pods_KEmptyTable
+@end
